5fbe2b4e231d343390d427ae9ea92268779f8fb7
Daniel George <danielgeorgewadea@gmail.com>
2025-04-29 13:05:44 +0300

    Initial commit

---
+++ src/client/java/me/daniel/marvelousmod/client/MarvelousmodClient.java
@@ -0,0 +1,10 @@
+package me.daniel.marvelousmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MarvelousmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/daniel/marvelousmod/Marvelousmod.java
@@ -0,0 +1,10 @@
+package me.daniel.marvelousmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Marvelousmod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
